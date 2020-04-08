7a7056d6d02f60c1c3a04733c09815123515917c
Keep Calm <maslevian@yandex.ru>
2020-04-09 00:19:55 +0300

    Add robot in the random cell function added

--- script.js
+++ script.js
@@ -37,6 +37,34 @@ class Grid {
 }
 
 
+function add_robot_random(ctx, grid, img) {
+	x = Math.floor(Math.random() * grid.M);
+	y = Math.floor(Math.random() * grid.N);
+
+	ctx.drawImage(img, grid.Indent + grid.Cell_size * x,
+		grid.Indent + grid.Cell_size * y, grid.Cell_size,
+		grid.Cell_size);
+}
+
+
+function add_rect_random(ctx, grid) {
+	x = Math.floor(Math.random() * grid.M);
+	y = Math.floor(Math.random() * grid.N);
+	console.log(x);
+	console.log(y);
+	
+	x_rect = grid.Indent + grid.Cell_size * x;
+	y_rect = grid.Indent + grid.Cell_size * y;
+	console.log(x_rect);
+	console.log(y_rect);
+	let s = `M ${x_rect} ${y_rect} h ${grid.Cell_size}
+		v ${grid.Cell_size} h ${-grid.Cell_size} Z`;
+	console.log(s);
+	let p = new Path2D(s);
+	ctx.fill(p);
+}
+
+
 function start() {
 	const canvas = document.getElementById('canvas');
 	const ctx = canvas.getContext('2d');
@@ -45,9 +73,9 @@ function start() {
 
 	var img = new Image();
 	img.src = 'img/robot.png';
-	img.onload = function() {
-		ctx.drawImage(img, grid.Indent, grid.Indent,
-			grid.Cell_size, grid.Cell_size);
+	
+	document.getElementById('change_btn').onclick = function() {
+		add_robot_random(ctx, grid, img);
 	}
 
 }
